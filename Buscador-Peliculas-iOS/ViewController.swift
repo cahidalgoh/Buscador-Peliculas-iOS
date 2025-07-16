1dd090ec7717fa9af9e8bcb5de3d553585265737
Carlos Hidalgo <carlos.hidalgoh@hotmail.com>
2025-07-16 16:24:45 +0200

    Initial Commit

---
+++ Buscador-Peliculas-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Buscador-Peliculas-iOS
+//
+//  Created by Tardes on 16/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
